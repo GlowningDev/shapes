5af67d3ecd9ac5a9fa557a1eef7be48c0ab982d8
Jouullye <103248197+Jouullye@users.noreply.github.com>
2022-05-02 16:52:37 +0200

    Suite du Visuel (Barre de Menu (Edit) + icons))

--- src/graphics/shapes/ui/MenuBar.java
+++ src/graphics/shapes/ui/MenuBar.java
@@ -28,16 +28,62 @@ public class MenuBar extends JFrame {
         menuFile.add(itemNew);
         itemNew.setMnemonic('N');
         itemNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,KeyEvent.CTRL_DOWN_MASK));
+        itemNew.setIcon(new ImageIcon("icons/new.png"));
+
 
         JMenuItem itemOpen= new JMenuItem("Ouvrir...");
         menuFile.add(itemOpen);
         itemOpen.setMnemonic('O');
-        itemNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,KeyEvent.CTRL_DOWN_MASK));
+        itemOpen.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,KeyEvent.CTRL_DOWN_MASK));
+        itemOpen.setIcon(new ImageIcon("icons/open.png"));
+
 
         JMenuItem itemSave= new JMenuItem("Sauvegarder");
         menuFile.add(itemSave);
         itemSave.setMnemonic('S');
-        itemNew.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,KeyEvent.CTRL_DOWN_MASK));
+        itemSave.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S,KeyEvent.CTRL_DOWN_MASK));
+        itemSave.setIcon(new ImageIcon("icons/save.png"));
+
+        JMenu menuEdit= new JMenu ("Edit");
+        menuBar.add(menuEdit);
+        menuEdit.setMnemonic('E');
+
+
+        JMenuItem itemUndo= new JMenuItem("Undo");
+        menuEdit.add(itemUndo);
+        itemUndo.setMnemonic('U');
+        itemUndo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z,KeyEvent.CTRL_DOWN_MASK));
+        itemUndo.setIcon(new ImageIcon("icons/undo.png"));
+
+
+        JMenuItem itemRedo=new JMenuItem("Redo");
+        menuEdit.add(itemRedo);
+        itemRedo.setMnemonic('R');
+        itemRedo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U,KeyEvent.CTRL_DOWN_MASK));
+        itemRedo.setIcon(new ImageIcon("icons/redo.png"));
+
+
+        JMenuItem itemCopy= new JMenuItem("Copy");
+        menuEdit.add(itemCopy);
+        itemCopy.setMnemonic('C');
+        itemCopy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C,KeyEvent.CTRL_DOWN_MASK));
+        itemCopy.setIcon(new ImageIcon("icons/copy.png"));
+
+
+        JMenuItem itemCut= new JMenuItem("Cut");
+        menuEdit.add(itemCut);
+        itemCut.setMnemonic('C');
+        itemCut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,KeyEvent.CTRL_DOWN_MASK));
+        itemCut.setIcon(new ImageIcon("icons/cut.png"));
+
+
+        JMenuItem itemPaste= new JMenuItem("Paste");
+        menuEdit.add(itemPaste);
+        itemPaste.setMnemonic('P');
+        itemPaste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V,KeyEvent.CTRL_DOWN_MASK));
+        itemPaste.setIcon(new ImageIcon("icons/paste.png"));
+
+
 
         return menuBar;
     }
