4ca5256f0271625877c414cee707dad82fd2b913
Jouullye <103248197+Jouullye@users.noreply.github.com>
2022-05-08 18:51:48 +0200

    ToolBar: bouton pour choisir la couleur (methode chooseColor)

--- src/graphics/shapes/ui/ToolBar.java
+++ src/graphics/shapes/ui/ToolBar.java
@@ -14,68 +14,73 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.Iterator;
 
-public class ToolBar {
+public class ToolBar<container> {
     Editor editor;
 
-    public ToolBar(Editor editor){
+    public ToolBar(Editor editor) {
         this.editor = editor;
     }
 
-    public JToolBar createToolBar(){
-        JToolBar toolBar= new JToolBar();
-        JButton buttonNew=new JButton(new ImageIcon("icons/new.png"));
+    public JToolBar createToolBar() {
+        JToolBar toolBar = new JToolBar();
+        JButton buttonNew = new JButton(new ImageIcon("icons/new.png"));
         toolBar.add(buttonNew);
 
-        JButton buttonSquare=new JButton(new ImageIcon("icons/square.png"));
+        JButton buttonSquare = new JButton(new ImageIcon("icons/square.png"));
         buttonSquare.setToolTipText("Forme: Square");
         toolBar.add(buttonSquare);
 
-        JButton buttonRectangle=new JButton(new ImageIcon("icons/rectangle.png"));
+        JButton buttonRectangle = new JButton(new ImageIcon("icons/rectangle.png"));
         buttonRectangle.setToolTipText("Forme: Rectangle");
         toolBar.add(buttonRectangle);
 
-        JButton buttonCircle=new JButton(new ImageIcon("icons/circle.png"));
+        JButton buttonCircle = new JButton(new ImageIcon("icons/circle.png"));
         buttonCircle.setToolTipText("Forme: Cercle");
         toolBar.add(buttonCircle);
 
-        JButton buttonTriangle=new JButton(new ImageIcon("icons/triangle.png"));
+        JButton buttonTriangle = new JButton(new ImageIcon("icons/triangle.png"));
         buttonTriangle.setToolTipText("Forme: Triangle");
         toolBar.add(buttonTriangle);
 
-        JButton buttonFree=new JButton(new ImageIcon("icons/pentagon.png"));
+        JButton buttonFree = new JButton(new ImageIcon("icons/pentagon.png"));
         buttonFree.setToolTipText("Forme: Polygone Libre");
         toolBar.add(buttonFree);
 
         toolBar.addSeparator();
 
-        JButton buttonText=new JButton(new ImageIcon("icons/text.png"));
+        JButton buttonText = new JButton(new ImageIcon("icons/text.png"));
         buttonCircle.setToolTipText("Ecrire le texte");
         toolBar.add(buttonText);
 
-        JButton buttonDraw=new JButton(new ImageIcon("icons/draw.png"));
+        JButton buttonDraw = new JButton(new ImageIcon("icons/draw.png"));
         buttonCircle.setToolTipText("Dessin libre");
         toolBar.add(buttonDraw);
 
-        JButton buttonErase=new JButton(new ImageIcon("icons/gomme.png"));
+        JButton buttonErase = new JButton(new ImageIcon("icons/gomme.png"));
         buttonErase.setToolTipText("Gomme");
         toolBar.add(buttonErase);
         buttonErase.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                        SCollection coll = editor.sview.getModel();
-                       coll.remove(coll);
-                    }
+                SCollection coll = editor.sview.getModel();
+                erase(coll);
+            }
         });
 
         toolBar.addSeparator();
 
-        JButton buttonPipette=new JButton(new ImageIcon("icons/pipette.png"));
+        JButton buttonPipette = new JButton(new ImageIcon("icons/pipette.png"));
         buttonPipette.setToolTipText("Pipette");
         toolBar.add(buttonPipette);
 
-        JButton buttonColor=new JButton(new ImageIcon("icons/color.png")); //Afficher une page avec un choix de couleur
-        buttonColor.addActionListener(this::ColorListener);
+        JButton buttonColor = new JButton(new ImageIcon("icons/color.png")); //Afficher une page avec un choix de couleur
         buttonColor.setToolTipText("Choissir la couleur");
+        buttonColor.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Color color=chooseColor();
+            }
+        });
         toolBar.add(buttonColor);
 
         toolBar.addSeparator();
@@ -84,10 +89,25 @@ public class ToolBar {
         return toolBar;
 
     }
-    //barre d'outil //JFileChooser (sauvegarder)
-    public void ColorListener(ActionEvent event){
-        JOptionPane.showMessageDialog(editor, "Systeme de couleur");
+
+
+    private Shape erase(SCollection coll) {
+        for (Iterator<Shape> it = coll.iterator(); it.hasNext(); ) {
+            Shape s = it.next();
+            SelectionAttributes sa = (SelectionAttributes) s.getAttributes(SelectionAttributes.ID);
+
+            if (sa.isSelected()) {
+                coll.remove(s);
+                return s;
+            }
+
+        }
+        return null;
     }
-
-
+    private Color chooseColor(){
+        JColorChooser colorChooser= new JColorChooser();
+        Color color=JColorChooser.showDialog(null,"Couleur à selectionner", Color.black);
+        JOptionPane.showMessageDialog(editor, "La couleur a bien été selectionné");
+        return color;
+    }
 }
