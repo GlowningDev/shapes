ad8ba623b35589761f86594067e9ae6dd53dbd00
Jouullye <103248197+Jouullye@users.noreply.github.com>
2022-05-08 17:39:51 +0200

    ToolBar: ajout des icons

--- src/graphics/shapes/ui/ToolBar.java
+++ src/graphics/shapes/ui/ToolBar.java
@@ -29,40 +29,40 @@ public class ToolBar extends JFrame {
         JButton buttonNew=new JButton(new ImageIcon("icons/new.png"));
         toolBar.add(buttonNew);
 
-        JButton buttonCircle=new JButton(new ImageIcon("icons/circle.svg"));
+        JButton buttonCircle=new JButton(new ImageIcon("icons/circle.png"));
         buttonCircle.setToolTipText("Forme: Cercle");
         toolBar.add(buttonCircle);
 
-        JButton buttonSquare=new JButton(new ImageIcon("icons/circle.svg"));
+        JButton buttonSquare=new JButton(new ImageIcon("icons/square.png"));
         buttonSquare.setToolTipText("Forme: Square");
         toolBar.add(buttonCircle);
 
-        JButton buttonRectangle=new JButton(new ImageIcon("icons/rectangle.svg"));
+        JButton buttonRectangle=new JButton(new ImageIcon("icons/rectangle.png"));
         buttonRectangle.setToolTipText("Forme: Rectangle");
 
         toolBar.add(buttonRectangle);
 
-        JButton buttonTriangle=new JButton(new ImageIcon("icons/new.png"));
+        JButton buttonTriangle=new JButton(new ImageIcon("icons/triangle.png"));
         buttonTriangle.setToolTipText("Forme: Triangle");
         toolBar.add(buttonTriangle);
 
-        JButton buttonFree=new JButton(new ImageIcon("icons/new.png"));
+        JButton buttonFree=new JButton(new ImageIcon("icons/pentagon.png"));
         buttonFree.setToolTipText("Forme: Polygone Libre");
         toolBar.add(buttonFree);
 
         toolBar.addSeparator();
 
-        JButton buttonErase=new JButton(new ImageIcon("icons/new.png"));
+        JButton buttonErase=new JButton(new ImageIcon("icons/gomme.png"));
         buttonErase.setToolTipText("Gomme");
         toolBar.add(buttonErase);
 
         toolBar.addSeparator();
 
-        JButton buttonPipette=new JButton(new ImageIcon("icons/new.png"));
+        JButton buttonPipette=new JButton(new ImageIcon("icons/pipette.png"));
         buttonPipette.setToolTipText("Pipette");
         toolBar.add(buttonPipette);
 
-        JButton buttonColor=new JButton(new ImageIcon("icons/new.png")); //Afficher une page avec un choix de couleur
+        JButton buttonColor=new JButton(new ImageIcon("icons/color.png")); //Afficher une page avec un choix de couleur
         buttonColor.addActionListener(this::ColorListener);
         buttonColor.setToolTipText("Choissir la couleur");
         toolBar.add(buttonColor);
